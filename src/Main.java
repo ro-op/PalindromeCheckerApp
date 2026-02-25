e3b0b8a532ffda7cfb415a8742934d78990c553e
ro-op <rj6316@srmist.edu.in>
2026-02-25 16:02:47 +0530

    Use Case 2: Print a Hardcoded Palindrome Result

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
 public class Main {
+
     public static void main(String[] args) {
+
+        // UC1: Welcome Message
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        System.out.println("System initialized successfully.\n");
+
+        // UC2: Hardcoded Palindrome Check
+        String word = "madam";
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Result Output
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
