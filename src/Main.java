6a320d1001305120880feab34bd6593547df7537
ro-op <rj6316@srmist.edu.in>
2026-02-25 16:20:41 +0530

    UC6

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,34 @@
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
-public class Main{
+
+public class Main {
+
     public static void main(String[] args) {
+
         // UC1: Welcome Message
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully.\n");
 
-        // UC5: Stack-Based Palindrome Check
+        // UC6: Queue + Stack Based Palindrome Check
         String word = "madam";
 
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
-        // Push characters into stack
+        // Enqueue and Push characters
         for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+            char ch = word.charAt(i);
+            queue.add(ch);     // FIFO
+            stack.push(ch);    // LIFO
         }
 
         boolean isPalindrome = true;
 
-        // Pop characters and compare
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != stack.pop()) {
+        // Compare dequeue (queue) vs pop (stack)
+        while (!queue.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
