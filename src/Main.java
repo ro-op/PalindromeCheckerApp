f6860a09ff533d1d5b7c55f68072874c757f4522
ro-op <rj6316@srmist.edu.in>
2026-02-25 15:58:15 +0530

    UC1: Add welcome message module(4)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public class UseCase1PalindromeCheckerApp {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
